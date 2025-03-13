5bdeab89c9b9efc2f9a2c51075d72ceb00b038a2
brianpimentel <brianepimentel@gmail.com>
2025-03-13 15:21:48 -0400

    Projectile WIP

---
+++ Source/Weapons/Public/NotHaloProjectile.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NotHaloProjectile.generated.h"
+
+UCLASS()
+class WEAPONS_API ANotHaloProjectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ANotHaloProjectile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
